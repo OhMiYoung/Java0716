a26840d45aa3254680eaf82817f08f9051c0e1a4
user <user@204-pro1-t>
2021-07-16 10:38:03 +0900

    PizzaExam 업로드

---
+++ PizzaExam/src/Circle.java
@@ -0,0 +1,12 @@
+
+public class Circle {
+	protected int size;  // size변수 protected로 선언
+    // size를 초기화시키는 생성자
+
+	public Circle(int size) {
+		this.size = size;
+	}
+
+
+
+}
---
+++ PizzaExam/src/Pizza.java
@@ -0,0 +1,21 @@
+
+public class Pizza extends Circle{     // Circle을 상속받음
+    private String name;// name 변수 private으로 선언
+
+    
+    // name과 size를 초기화시키는 생성자
+    // super 사용
+    public Pizza(int size, String name) {
+    	super(size);
+    	this.name = name;
+    }
+
+@Override
+public String toString() {
+
+return "피자의 종류:" + name + ", 피자의 크기:" + size;
+}
+
+
+
+}
